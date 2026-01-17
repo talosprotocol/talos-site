a8cdd4052cea21658d8b5928d63034a4d06164e9
Nilesh Chakraborty <nileshchakraborty@gmail.com>
2026-01-17 00:37:32 -0600

    fix(site): add basePath for github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
   output: 'export',
+  // Only set basePath if configuring for GitHub Pages deployment
+  basePath: isGithubActions ? '/talos' : undefined,
   images: {
     unoptimized: true,
   },
