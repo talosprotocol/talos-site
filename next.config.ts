e6d51a839b2aa76756b748d0bb6752d378611340
Nilesh Chakraborty <nileshchakraborty@gmail.com>
2026-01-17 00:31:45 -0600

    chore: configure static export for gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.talosprotocol.com",
-          },
-        ],
-        destination: "https://talosprotocol.com/:path*",
-        permanent: true,
-      },
-    ];
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
 };
 
